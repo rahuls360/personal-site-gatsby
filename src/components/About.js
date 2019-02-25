3af749a3f254a6cf71f76f280bb6b04069a7c03a
Rahul Makhija <rahuls360@gmail.com>
2019-02-25 12:44:39 +0530

    add about section

---
+++ src/components/About.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+const About = () => (
+    <div className="row my-5">
+    <h3 className="text-center">About</h3>
+        <div className="container">
+            <div className="col-md-6">
+                <img src="https://picsum.photos/600/600?random" alt="" className="img-responsive"/>
+            </div>
+            <div className="col-md-6">
+                <p className="text-justify">
+                    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Eius, eaque! Dolor beatae sint voluptatum impedit soluta neque dolorem rem? Culpa quam a voluptates repudiandae ea sint, labore nihil inventore vel!
+                </p>
+            </div>
+        </div>
+    </div>
+)
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from '../components/Header';
 import Hero from '../components/Hero';
+import About from '../components/About';
 import Interest from '../components/Interest';
 import Contact from '../components/Contact';
 import Footer from '../components/Footer';
@@ -17,6 +18,7 @@ export default () => (
     <>
     <Header></Header>
     <Hero></Hero>
+    <About></About>
     <div className="row my-5">
     <h3 className="text-center">Interests</h3>
         <div className="container">
